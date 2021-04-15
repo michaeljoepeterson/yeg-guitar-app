94f1536cbcc89955d3d4e07704b760ec6c7d82c3
michael peterson <michaeljoepeterson@gmail.com>
2021-04-14 20:48:10 -0600

    added pagination to results table

--- src/components/sub-components/lesson-view-table.js
+++ src/components/sub-components/lesson-view-table.js
@@ -13,11 +13,21 @@ import { Lesson } from '../../models/lesson';
 
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import TablePagination from '@material-ui/core/TablePagination';
+import {Pager} from '../../helpers/pager';
 import './styles/table-styles.css';
 
 export function LessonViewTable(props){
 
-    const [hoveredRow,setHoveredRow] = useState(null)
+    const [hoveredRow,setHoveredRow] = useState(null);
+    const [page, setPage] = useState(0);
+    const [resultNum, setresultNum] = useState(30);
+    const pager = new Pager({
+        items:props.lessons,
+        resultPerPage:resultNum
+    });
+    const rowsPerPage = [10,20,30,50,70,100];
+    const visisbleLessons = pager.getPage(page);
 
     const setLesson = (lesson) => {
         if(props.user.level <= 1 || (props.user.id === lesson.teacher.id)){
@@ -25,14 +35,14 @@ export function LessonViewTable(props){
             props.dispatch(setSelectedLesson(lesson));
             props.history.push(`/edit-lesson/${lesson.id}`);
         }
-    }
+    };
 
     const studentClicked = (student) => {
         console.log('student clicked: ',student);
         if(props.studentClicked){
             props.studentClicked(student);
         }
-    }
+    };
 
     const buildStudentSpans = (students) =>{
         let spans = students.map((student,i) => {
@@ -40,7 +50,7 @@ export function LessonViewTable(props){
         });
 
         return spans;
-    }
+    };
 
     const dateClicked = (date) =>{
         console.log('date clicked',date);
@@ -48,23 +58,32 @@ export function LessonViewTable(props){
         if(props.dateClicked){
             props.dateClicked(date);
         }
-    }
+    };
 
     const teacherClicked = (teacher) =>{
         console.log('teacher clicked',teacher);
         if(props.teacherClicked){
             props.teacherClicked(teacher);
         }
-    }
-
+    };
+    /*
     const enterRow = (row) => {
         console.log('row entered');
         setHoveredRow(row);
-    }
+    };
 
     const exitRow = () => {
         setHoveredRow(null);
-    }
+    };
+    */
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setresultNum(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     const buildTable = (passedLessons) =>{
         let lessons = passedLessons.map(lesson => new Lesson(lesson));
@@ -117,26 +136,35 @@ export function LessonViewTable(props){
         }
 
         return(<TableContainer component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Lesson Type</TableCell>
-                    <TableCell>Notes</TableCell>
-                    <TableCell>Students</TableCell>
-                    <TableCell>Teacher</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows}
-                </TableBody>
-            </Table>
+                <Table aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Date</TableCell>
+                        <TableCell>Lesson Type</TableCell>
+                        <TableCell>Notes</TableCell>
+                        <TableCell>Students</TableCell>
+                        <TableCell>Teacher</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    component="div"
+                    count={props.lessons.length}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    rowsPerPage={resultNum}
+                    rowsPerPageOptions={rowsPerPage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                    />
             </TableContainer>
         );
     }
     let table = null;
     if(props.lessons && props.lessons.length > 0){
-        table = buildTable(props.lessons);
+        table = buildTable(visisbleLessons);
     }
 
     return(
